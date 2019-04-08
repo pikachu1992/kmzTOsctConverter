597b04154c92332ab65bef6f7a4982df2c954822
Tiago Vicente <tmavicente@gmail.com>
2019-04-08 13:42:12 +0100

    fix marking tags

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ SECTOR_COLORS = {
     "_Background" : COLOR_PREFIX + "ApronSurface",
     "_Runway" : COLOR_PREFIX + "RunwayConcrete",
     "Marking" : COLOR_PREFIX + "RunwayBorder",
-    "Marking_" : COLOR_PREFIX + "ParkPos",
+    "ApronMark" : COLOR_PREFIX + "ParkPos",
     "Taxiway_" : COLOR_PREFIX + "Taxiway",
     "_stop" : COLOR_PREFIX + "Stopbar",
     "Stand_" : COLOR_PREFIX + "Taxiway",
