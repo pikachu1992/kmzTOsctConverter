7a4f63d37c5e8319f7459a069245f08f8e387410
Tiago Vicente <tmavicente@gmail.com>
2019-04-08 02:35:19 +0100

    fix all format bugs3

--- convert_geoJson_to_sct.py
+++ convert_geoJson_to_sct.py
@@ -13,89 +13,84 @@ def geoJSON_to_sct(filepath):
     with open(filepath, 'r') as file:
         data = json.loads(file.read())
     
-    output = list()
+    def convert_LON(c):
+        if c < 0:
+            degrees, minutes, seconds = decdeg2dms(c)
+            if degrees < 10:
+                return "W00{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds)
+            else:
+                return "W0{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds)
+        else:
+            degrees, minutes, seconds = decdeg2dms(c)
+            return "E{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
 
-    for item in data["features"]:
-        for color in SECTOR_COLORS:
-            if color in item["properties"]["name"] and color != "Taxiway_":
-                output.append("{0}".format(SECTOR_COLORS[color]))
-        output.append("; - {0}".format(item["properties"]["name"]))
-        t = 0
-        y = 0
-        string_parse = ""
-        for geo in item["geometry"]["coordinates"]:
-            parse = [x for x in geo]           
-            for i in parse:
-                if type(i) == list:
-                    string_parse = ""
-                    i.pop(2)
-                    t = 0
-                    for c in i[:2]:
-                        if t == 0:
-                            if c < 0:
-                                degrees, minutes, seconds = decdeg2dms(c)
-                                if degrees < 10:
-                                    i[t] = "W00{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds)
-                                else:
-                                    i[t] = "W0{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds)
-                            else:
-                                degrees, minutes, seconds = decdeg2dms(c)
-                                i[t] = "E{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
+    def convert_LAT(c):
+        if c < 0:
+            degrees, minutes, seconds = decdeg2dms(c)
+            return "S{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
+        else:
+            degrees, minutes, seconds = decdeg2dms(c)
+            return "N0{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
 
-                        if t == 1:
-                            if c < 0:
-                                degrees, minutes, seconds = decdeg2dms(c)
-                                i[t] = "S{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
-                            else:
-                                degrees, minutes, seconds = decdeg2dms(c)
-                                i[t] = "N0{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds)
-                        t+=1
-                    output.append(str(i[::-1]).replace('[', '').replace(']', '').replace(',', '').replace("'", ""))
-                else:
-                    if len(parse) > 2:
-                        parse.pop(2)
-                    if type(i) == float:
-                        str_coord = []
-                        if len(string_parse.split()) == 4:
-                            if i < 0:
-                                degrees, minutes, seconds = decdeg2dms(i)
-                                str_coord.append("S{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds))
-                            else:
-                                degrees, minutes, seconds = decdeg2dms(i)
-                                str_coord.append("N0{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds))   
+    def convert_regions():
+        output = list()
 
-                            if string_parse.split()[0][0] == 'W':
-                                string = string_parse.split()[:2][::-1] + string_parse.split()[2:][::-1]
-                            else:
-                                string = string_parse.split()[:2] + string_parse.split()[2:]
-                            string_parse = str(string).replace('[', '').replace(']', '').replace(',', '').replace("'", "") + " {0}".format(SECTOR_COLORS["Taxiway_"])
-                            output.append(string_parse)
-                            string_parse = ""
-                        else:
-                            data = parse
-                            if i < 0:
-                                degrees, minutes, seconds = decdeg2dms(i)
-                                if degrees < 10:
-                                    str_coord.append("W00{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds))
-                                else:
-                                    str_coord.append("W0{0:.0f}.{1:.0f}.{2:.3f}".format((degrees*-1), minutes, seconds))
-                            else:
-                                if i < 0:
-                                    degrees, minutes, seconds = decdeg2dms(i)
-                                    str_coord.append("S{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds))
-                                else:
-                                    degrees, minutes, seconds = decdeg2dms(i)
-                                    str_coord.append("N0{0:.0f}.{1:.0f}.{2:.3f}".format(degrees, minutes, seconds))
-                            
-                            string_parse += str(str_coord).replace('[', '').replace(']', '').replace(',', '').replace("'", "")+" "
-                            
+        for item in data["features"]:
+            for color in SECTOR_COLORS:
+                if color in item["properties"]["name"] and color != "Taxiway_" and color != "Stand_":
+                    output.append("{0}".format(SECTOR_COLORS[color]))
+                    output.append("; - {0}".format(item["properties"]["name"]))
+            t = 0
+            if item["properties"]["name"] != "Taxiway_" and item["properties"]["name"] != "Stand_":
+                for geo in item["geometry"]["coordinates"]:
+                    parse = [x for x in geo]          
+                    for i in parse:
+                        if type(i) == list:                      
+                            i.pop(2)
+                            t = 0
+                            for c in i[:2]:
+                                if t == 0:
+                                    i[t] = convert_LON(c)
+                                if t == 1:
+                                    i[t] = convert_LAT(c)
+                                t+=1
+                            output.append(str(i[::-1]).replace('[', '').replace(']', '').replace(',', '').replace("'", ""))
+        return output
 
+    def convert_taxiways_and_stands():
+        output = list()
+        
 
+        for item in data["features"]:
             
-                        
+            #if (item["properties"]["name"] == "Taxiway_W_B"):
+            if ("Taxiway_" in item["properties"]["name"] or "Stand_" in item["properties"]["name"]):
+                output.append("; - {0}".format(item["properties"]["name"]))
+                t = 0
+                list_to_four = list()
+                for geo in item["geometry"]["coordinates"]:
+                    geo.pop(2)
+                    if t == 0 and len(list_to_four) == 2:
+                        output.append(" ".join(list_to_four))
+                    if len(list_to_four) >= 1 and len(list_to_four) / (len(list_to_four) / 2) == 2 and t != 0:
+                        lat = convert_LAT(geo[::-1][0])
+                        lng = convert_LON(geo[::-1][1])
+                        list_to_four.append((lat, lng))
+                        if "Taxiway_" in item["properties"]["name"]:
+                            output.append("{} {} {}".format(list_to_four[t-1], list_to_four[t], SECTOR_COLORS["Taxiway_"]).replace('(', '').replace(')', '').replace(',', '').replace("'", ""))
+                        elif "Stand_" in item["properties"]["name"]:
+                            output.append("{} {} {}".format(list_to_four[t-1], list_to_four[t], SECTOR_COLORS["Stand_"]).replace('(', '').replace(')', '').replace(',', '').replace("'", ""))
+                    else:
+                        lat = convert_LAT(geo[::-1][0])
+                        lng = convert_LON(geo[::-1][1])
+                        list_to_four.append((lat, lng))
+                    
+                    t += 1
+                              
+        return output
 
-                            
-                            
+    output = convert_regions()
+    output += convert_taxiways_and_stands()
 
     with open('converted/' + filepath.split("/")[1].split(".")[0] + '.sct', 'w') as file:
         file.write("\n".join(output))
