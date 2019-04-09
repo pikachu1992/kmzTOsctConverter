0b97131753ed9602cfdb08214c321b91c71b591b
Tiago Vicente <tmavicente@gmail.com>
2019-04-09 16:44:55 +0100

    fix on return

--- web.py
+++ web.py
@@ -47,7 +47,7 @@ def upload_file():
         # check if the post request has the file part
         if 'hh' not in request.files:
             flash('No file part')
-            return json.dumps("No have file insert one file first")
+            return json.loads("No have file insert one file first")
         file = request.files['hh']
         # if user does not select file, browser also
         # submit an empty part without filename
@@ -59,7 +59,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return convert(file.filename)
         else:
-            return json.dumps("File extension not allowed. Send only .kmz files")
+            return json.loads("File extension not allowed. Send only .kmz files")
 
 
 
